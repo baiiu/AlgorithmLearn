82cab63ed4699ad986ac1fa4e3ab9abbb0215f1d
baiiu <baiiu@foxmail.com>
2017-04-26 23:12:47 +0800

    topology sort

--- algorithm/src/main/java/com/baiiu/DAG/DFSTopologicalSort.java
+++ algorithm/src/main/java/com/baiiu/DAG/DFSTopologicalSort.java
@@ -1,6 +1,7 @@
 package com.baiiu.DAG;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -48,7 +49,7 @@ class DFSTopologicalSort {
             dfs(vertex, mSortResult, mSortTmpMarked);
         }
 
-        //Collections.reverse(mSortResult);
+        Collections.reverse(mSortResult);
         return mSortResult;
     }
 
