72bdeccfac252be481477570db9d7e38799543b2
baiiu <baiiu@foxmail.com>
2017-06-01 11:59:48 +0800

    sth

--- 剑指offer/src/main/java/com/example/Question08_MinNumberInRotatedArray.java
+++ 剑指offer/src/main/java/com/example/Question08_MinNumberInRotatedArray.java
@@ -9,18 +9,82 @@ class Question08_MinNumberInRotatedArray {
 
     /**
      * 把一个数组最开始的若干个元素搬到数组的末尾，称之为 旋转数组。
-     *
+     * <p>
      * Question:
      * 输入一个递增排序的数组的一个旋转，输出该旋转数组的最小值。
      */
     static void test() {
-        int[] rotatedArray = new int[] { 3, 4, 5, 6, 7, 1, 2 };
+//        int[] rotatedArray = new int[]{3, 4, 5, 6, 7, 0, 1, 3};
+        int[] rotatedArray = new int[]{1, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1};
 
-        minValue(rotatedArray);
+        foreachToSearchMinValue(rotatedArray);
+        middleSearchMinValue_Recursive(rotatedArray, 0, rotatedArray.length - 1);
+        middleSearchMinValueWhile(rotatedArray);
     }
 
-    private static void minValue(int[] rotatedArray) {
-        if (rotatedArray == null || rotatedArray.length == 1) return;
+    private static void middleSearchMinValueWhile(int[] rotatedArray) {
+        int point1 = 0, point2 = rotatedArray.length - 1, middle = 0;
+
+//        while (rotatedArray[point1] >= rotatedArray[point2]) {
+        while (true) {
+            if (point2 - point1 == 1) {
+                middle = point2;
+                break;
+            }
+
+            middle = (point1 + point2) / 2;
+
+            if (rotatedArray[point1] == rotatedArray[point2] && rotatedArray[point1] == rotatedArray[middle]) {
+                foreachToSearchMinValue(rotatedArray);
+                return;
+            }
+
+            if (rotatedArray[middle] >= rotatedArray[point1]) {
+                point1 = middle;
+            } else if (rotatedArray[middle] <= rotatedArray[point2]) {
+                point2 = middle;
+            }
+        }
+
+        System.out.println(middle + ", " + rotatedArray[middle]);
+    }
+
+    private static void middleSearchMinValue_Recursive(int[] rotatedArray, int point1, int point2) {
+        if (rotatedArray == null || rotatedArray.length == 0) return;
+        /*
+            二分查找
+
+            旋转数组，头部一定大于或等于尾部
+
+            point1指向开头，point2指向末尾，middle就是中间
+
+            如果 *point1 == *point2 == *middle，则无法区分，只能遍历
+
+            如果*middle大于等于*piont1，最小值在middle后面，或这个值就是最小值
+            如果*middle小于等于*point2，最小值在middle前面，或这个值就是最小值
+         */
+
+        if ((point2 - point1 == 1)) {
+            System.out.println(point2 + ", " + rotatedArray[point2]);
+            return;
+        }
+
+        int middle = (point1 + point2) / 2;
+
+        if (rotatedArray[point1] == rotatedArray[point2] && rotatedArray[point1] == rotatedArray[middle]) {
+            foreachToSearchMinValue(rotatedArray);
+            return;
+        }
+
+        if (rotatedArray[middle] >= rotatedArray[point1]) {
+            middleSearchMinValue_Recursive(rotatedArray, middle, point2);
+        } else if (rotatedArray[middle] <= rotatedArray[point2]) {
+            middleSearchMinValue_Recursive(rotatedArray, point1, middle);
+        }
+    }
+
+    private static void foreachToSearchMinValue(int[] rotatedArray) {
+        if (rotatedArray == null || rotatedArray.length == 0) return;
 
         /*
             遍历：O(n),第一个变小的值就是最小值
---
+++ 剑指offer/src/main/java/com/example/Question09_Fibonacci.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * author: baiiu
+ * date: on 17/6/1 11:57
+ * description:
+ */
+class Question09_Fibonacci {
+
+    /**
+     * 求斐波那契数列的第n项值
+     * <p>
+     * <p>
+     * f(0) = 0
+     * f(1) = 1
+     * f(n) = f(n - 1) + f(n - 2)
+     */
+    static void test() {
+
+    }
+
+}
