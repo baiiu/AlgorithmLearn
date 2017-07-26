5703a8b3a4e41afb5212533d49b95450195c42bb
baiiu <baiiu@foxmail.com>
2017-07-26 11:01:35 +0800

    sth

--- 剑指offer/src/main/java/com/example/Question32_.java
+++ 剑指offer/src/main/java/com/example/Question32_.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * author: baiiu
  * date: on 17/7/24 11:23
@@ -18,7 +23,45 @@ class Question32_ {
      */
     static void test() {
 
+        countOneNumbers(123);
 
     }
+
+
+    /*
+        1. 遍历[1,n]，%10计算每个位数上是否为1
+
+        O(nlogn) logn为n的位数，用最大的循环来表示
+     */
+    private static void countOneNumbers(int targetNumber) {
+        if (targetNumber <= 1) return;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        int sumOne = 0;
+        for (int i = 1; i <= targetNumber; ++i) {
+            int number = i;
+
+            while (number / 10 != 0) { //该循环用于计算每一位数字
+                if (number % 10 == 1) {
+                    hashSet.add(i);
+                    ++sumOne;
+                }
+
+                number /= 10;
+            } // 循环完后number只剩一个数字，为最高位
+
+            if (number == 1) {
+                hashSet.add(i);
+                ++sumOne;
+            }
+        }
+
+        List<Integer> list = new ArrayList<Integer>(hashSet);
+        Collections.sort(list);
+        System.out.println(list.toString());
+        System.out.println(sumOne);
+    }
+
 
 }
