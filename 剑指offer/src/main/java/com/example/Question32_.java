4410ca128cf2e74d1d006f26762ed25ef3993ddb
baiiu <baiiu@foxmail.com>
2017-07-27 13:00:23 +0800

    sth

--- 剑指offer/src/main/java/com/example/Question32_.java
+++ 剑指offer/src/main/java/com/example/Question32_.java
@@ -22,9 +22,94 @@ class Question32_ {
      * 1共出现5次。
      */
     static void test() {
+        int number = 12;
 
-        countOneNumbers(123);
+        countOneNumbers(number);
+        countOneNumbers2(number);
 
+//        int[] a = new int[]{1, 2};
+//        System.out.println(atoi(a, 1));
+//        System.out.println(atoi2(a, 1));
+    }
+
+    /*
+        2. 分析数字的规律
+     */
+    private static void countOneNumbers2(int targetNumber) {
+        if (targetNumber <= 0) return;
+
+        String targetNumberString = String.valueOf(targetNumber);
+        int[] numbers = new int[targetNumberString.length()];
+
+        for (int i = 0; i < targetNumberString.length(); ++i) {
+            numbers[i] = targetNumberString.charAt(i) - '0';
+        }
+
+        System.out.println(numberOfOne(numbers, 0)); //从最高位开始处理
+    }
+
+    private static int numberOfOne(int[] numbers, int currentIndex) {
+        if (numbers == null || numbers.length == 0 || currentIndex > numbers.length - 1 || currentIndex < 0)
+            return 0;
+
+        int first = numbers[currentIndex]; //待处理的数字，最高位
+        int length = numbers.length - currentIndex; //要处理的数字的位数
+
+        if (length == 1 && first == 0) return 0;
+        if (length == 1 && first > 0) return 1;
+
+        int numFirstDigit = 0;
+
+        // 21345,first=2,length=5,共有10000个 10^4
+        if (first > 1) {
+            numFirstDigit = powerBase10(length - 1);
+        } else if (first == 1) {
+            // 12345,first=1,length=5,共有2345 + 1个
+            numFirstDigit = atoi(numbers, currentIndex + 1);
+        }
+
+        // 在[1346, 21345]中，这20000个数中
+        // 除了第一位以外的位置，即剩下的位置中1的个数为2*4*1000，全排列 2xxxx， 2*4*C10 1
+        int numOtherDigits = first * (length - 1) * powerBase10(length - 2);
+
+        // [1, 1345]，接下来index右移
+        int numRecursive = numberOfOne(numbers, currentIndex + 1);
+
+
+        return numFirstDigit + numOtherDigits + numRecursive;
+    }
+
+    // 最高位大于1时，共有多少个 10000~19999
+    private static int powerBase10(int length) {
+        int result = 1;
+        for (int i = 0; i < length; ++i) {
+            result *= 10;
+        }
+
+        return result;
+    }
+
+    private static int atoi(int[] numbers, int i) {
+        int result = 0;
+        for (int j = i; j < numbers.length; j++) {
+            result = (result * 10 + numbers[j]);
+        }
+        return ++result;
+    }
+
+    // 12345
+    private static int atoi2(int[] numbers, int start) {
+        if (numbers == null || numbers.length == 0 || start < 0 || start > numbers.length - 1)
+            return 0;
+
+        int result = 0;
+        for (int i = start, length = numbers.length; i < length; ++i) {
+            int number = numbers[i];
+
+            result += number * Math.pow(10, length - i - 1);
+        }
+
+        return ++result;
     }
 
 
