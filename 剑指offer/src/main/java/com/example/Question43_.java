558e37b3fe8c2e40e760f587818917e874e1f512
baiiu <baiiu@foxmail.com>
2017-09-09 12:38:42 +0800

    sth

--- 剑指offer/src/main/java/com/example/Question43_.java
+++ 剑指offer/src/main/java/com/example/Question43_.java
@@ -16,14 +16,48 @@ class Question43_ {
      * 输入n，打印出s的所有可能的值出现的概率。
      */
     static void test() {
-        int n = 2;
+        int n = 3;
 
         int[] arr = new int[n * 6 + 1];
         count(n, arr);
 
 
-        printSum(arr);
+//        printSum(arr, n);
+        printProbability(n);
     }
+
+    // 二维数组
+    private static void printProbability(int number) {
+        if (number < 1)
+            return;
+        int g_maxValue = 6;
+        int[][] probabilities = new int[2][];
+        probabilities[0] = new int[g_maxValue * number + 1];
+        probabilities[1] = new int[g_maxValue * number + 1];
+        int flag = 0;
+        for (int i = 1; i <= g_maxValue; i++) {
+            probabilities[0][i] = 1;
+        }
+
+        for (int k = 2; k <= number; ++k) {
+            for (int i = 0; i < k; ++i) {
+                probabilities[1 - flag][i] = 0;
+            }
+
+            for (int i = k; i <= g_maxValue * k; ++i) {
+                probabilities[1 - flag][i] = 0;
+                for (int j = 1; j <= i && j <= g_maxValue; ++j)
+                    probabilities[1 - flag][i] += probabilities[flag][i - j];
+            }
+            flag = 1 - flag;
+        }
+
+        int total = (int) Math.pow(g_maxValue, number);
+        for (int i = number; i <= g_maxValue * number; i++) {
+            System.out.println("点数: " + i + "\t,出现次数: " + probabilities[flag][i] + "\t, 概率: " + probabilities[flag][i] + "/" + total);
+        }
+    }
+
 
     /*
         1. 如何求出所有的和
@@ -38,9 +72,9 @@ class Question43_ {
     private static void count(int n, int[] arr) {
 
         // 每一次递归，都会进行一个加1
+
         while (n > 0) {
             --n;
-
 
             if (isNull(arr)) {
                 for (int i = 1; i < 7; ++i) {
@@ -50,23 +84,23 @@ class Question43_ {
             } else {
 
                 // 遍历上次的所有和，对所有和 + 相应的骰子数
-                for (int i = 1, length = countLength(arr); i < length; ++i) {
+                int originLength = countLength(arr);
+
+                for (int i = 1; i < originLength; ++i) {
                     if (arr[i] == 0) continue;
 
                     for (int j = 1; j < 7; ++j) {
-                        // 遍历上一次存入的值，开始对每个值加sum
-                        int key = arr[i] + j;
-
-                        if (arr[key] == 0) {
-                            arr[key] = 1;
-                        } else {
-                            ++arr[key];
-                        }
-
+                        int key = i + j;
+                        ++arr[key];
                     }
-
                 }
 
+                //其中对arr[1,length)多加了一个原本的1，需要减去
+                for (int i = 1; i < originLength; ++i) {
+                    if (arr[i] == 0) continue;
+
+                    --arr[i];
+                }
             }
         }
     }
@@ -85,20 +119,23 @@ class Question43_ {
     private static int countLength(int[] arr) {
         if (CommonUtil.isEmpty(arr)) return 0;
 
-        int index = 1;
+        int index = arr.length - 1;
 
-        while (arr[index] != 0) {
-            ++index;
+        while (arr[index] == 0) {
+            --index;
         }
 
-        return index;
+        return index + 1;
     }
 
-    private static void printSum(int[] arr) {
+    // 概率就是出现次数除以所有可能性 6^n
+    private static void printSum(int[] arr, int n) {
         if (CommonUtil.isEmpty(arr)) return;
 
+        int sum = (int) Math.pow(6, n);
+
         for (int i = 1, length = arr.length; i < length; i++) {
-            System.out.println("点数: " + i + "， 出现次数: " + arr[i]);
+            System.out.println("点数: " + i + "\t,出现次数: " + arr[i] + "\t, 概率: " + arr[i] + "/" + sum);
         }
 
     }
