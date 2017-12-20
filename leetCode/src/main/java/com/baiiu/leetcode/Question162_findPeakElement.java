10d8cd9fccffe52c93b57f0050f23932dcfc9b77
baiiu <baiiu@foxmail.com>
2017-12-20 22:32:06 +0800

    sth

---
+++ leetCode/src/main/java/com/baiiu/leetcode/Question162_findPeakElement.java
@@ -0,0 +1,23 @@
+package com.baiiu.leetcode;
+
+/**
+ * auther: baiiu
+ * time: 17/12/20 20 22:29
+ * description:
+ */
+class Question162_findPeakElement {
+
+    /**
+     * A peak element is an element that is greater than its neighbors.
+     * Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+     * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+     * You may imagine that num[-1] = num[n] = -∞.
+     * For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+     * <p>
+     * https://leetcode.com/problems/find-peak-element/description/
+     */
+    static void test() {
+
+    }
+
+}
