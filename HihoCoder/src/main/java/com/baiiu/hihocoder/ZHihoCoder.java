dbd8c8ac3eb059c8d3bb8dccdbd4d1acc6c4f853
baiiu <baiiu@foxmail.com>
2017-12-27 14:43:18 +0800

    sth

---
+++ HihoCoder/src/main/java/com/baiiu/hihocoder/ZHihoCoder.java
@@ -0,0 +1,16 @@
+package com.baiiu.hihocoder;
+
+/**
+ * author: baiiu
+ * date: on 17/12/16 15:13
+ * description:
+ */
+public class ZHihoCoder {
+
+    public static void main(String[] args) {
+
+        Question_1039.test();
+
+    }
+
+}
