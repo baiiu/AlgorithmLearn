396cc9d345cb256e406424e5ee8bc8aa8351f74f
baiiu <baiiu@foxmail.com>
2019-03-24 22:04:08 +0800

    sth

---
+++ leetCode/src/main/java/com/baiiu/leetcode/array/Q16_3Sum_Closest.java
@@ -0,0 +1,30 @@
+package com.baiiu.leetcode.array;
+
+/**
+ * auther: baiiu
+ * time: 19/3/23 23 21:52
+ * description:
+ */
+public class Q16_3Sum_Closest {
+
+    /**
+     * Given an array nums of n integers and an integer target,
+     * find three integers in nums such that the sum is closest to target.
+     * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+     * 题目要求可以假设只有一个
+     *
+     * example:
+     * Given array nums = [-1, 2, 1, -4], and target = 1.
+     * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+     */
+    public static void main(String[] args) {
+
+        int[] arr = new int[] { -1, 2, 1, -4 };
+        int target = 1;
+
+
+
+
+    }
+
+}
